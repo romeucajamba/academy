f37d31c13ddef5fd965783fc16a0c579ff99ad01
romeucajamba <romeucajamba@gmail.com>
2024-06-08 14:06:54 +0100

    config: prapre index to variable envoirements

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,1 +1,19 @@
 import 'dotenv';
+import { z } from 'zod';
+import { BadRequestError} from '../error/badrequest';
+
+
+const schemaEnv = z.object({
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    PORT: z.coerce.number().default(4000)
+})
+
+const _env = schemaEnv.safeParse(process.env)
+
+if (_env.success == false){
+    console.error('Variáveis de ambient inválida', _env.error.format())
+
+    throw new BadRequestError('Variáveis de ambient inválida')
+}
+
+export const env = _env.data
